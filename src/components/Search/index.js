06e13efb6f9101a29a094f5025ef495ada6058bd
Sebastián Díaz <sebastian88@gmail.com>
2021-03-08 03:13:00 -0300

    WIP 2 Search Bar

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Input, Div } from './styles';
-import Select from 'react-select';
+import { Input, Div, Select } from './styles';
+import SearchIcon from 'pokemon-assets/assets/img/symbols/ui-search.png';
 
 export const Search = () => {
   const options = [
@@ -11,8 +11,19 @@ export const Search = () => {
 
   return (
     <Div>
-      <Input type='text' placeholder='Busca en Poke Team Builder' />
-      <Select options={options} />
+      <Input
+        type='text'
+        placeholder='Busca en PokeTeamBuilder'
+        onFocus={(e) => e.target.placeholder = ''}
+        onBlur={(e) => e.target.placeholder = 'Busca en PokeTeamBuilder'}
+      >asd
+      </Input>
+      <img src={SearchIcon} alt='Search Icon' />
+      <Select
+        options={options}
+        classNamePrefix='search-select'
+        defaultValue={{ value: 'all', label: 'All' }}
+      />
     </Div>
   );
 };
--- src/components/Search/styles.js
+++ src/components/Search/styles.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
+import ReactSelect from 'react-select';
 
 export const Input = styled.input`
+  width: 350px;
   font-size: 16px;
   height: 2.5rem;
   border: none;
@@ -8,6 +10,7 @@ export const Input = styled.input`
   padding-inline-start: 52px;
   box-shadow: 0px -1px 63px -5px rgba(0,0,0,0.75);
   border-radius: 99px;
+  padding-top: 3px;
 `;
 
 export const Div = styled.div`
@@ -15,11 +18,10 @@ export const Div = styled.div`
   justify-content: center;
 `;
 
-// export const Select = styled.select`
-//   font-size: 16px;
-//   height: 2.5rem;
-//   border: none;
-//   box-shadow: 0px -1px 63px -5px rgba(0,0,0,0.75);
-//   border-radius: 99px;
-//   padding-inline-start: 20px;
-// `;
+export const Select = styled(ReactSelect)`
+  width: 250px;
+  margin-left: 25px;
+  & .search-select__control {
+    height: 2.5rem;
+    }
+`;
